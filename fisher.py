7ba2bd5a23e3e6accc8e652aec2d5782fe68c28b
eph <painy.eph@gmail.com>
2017-02-14 11:55:00 +0800

    code optimized

--- fisher.py
+++ fisher.py
@@ -1,11 +1,33 @@
-# eph 091204 created
-# eph 100713 fixed: "return 0.0, 0.0, 0.0" in single tests
-# eph 170214 using math.lgamma in Python 2.7+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: eph
 
 from math import log, exp
 
+try:
+
+    from math import lgamma  # Python 2.7+
+
+except ImportError:
+
+    _cache = {0: 0., 1: 0.}
+    def lgamma(z):
+        if z in _cache: return _cache[z]
+        x = 0.1659470187408462e-06 / (z + 7)
+        x += 0.9934937113930748e-05 / (z + 6)
+        x -= 0.1385710331296526 / (z + 5)
+        x += 12.50734324009056 / (z + 4)
+        x -= 176.6150291498386 / (z + 3)
+        x += 771.3234287757674 / (z + 2)
+        x -= 1259.139216722289 / (z + 1)
+        x += 676.5203681218835 / z
+        x += 0.9999999999995183
+        _cache[z] = x = log(x) - 5.58106146679532777 - z + (z-0.5)*log(z+6.5)
+        return x
+
 LN10 = log(10)
 NINF = float('-inf')
+
 
 # ======================== Full Test ========================
 
@@ -21,30 +43,24 @@ def mlnTest1(a, b, c, d):
     return mlnTest2(a, a+b, a+c, a+b+c+d)
 
 def mlnTest2(a, ab, ac, abcd):
+    if 0 > a or a > ab or a > ac or ab > abcd or ac > abcd: raise ValueError
     a_min = max(0, ab+ac-abcd)
     a_max = min(ab, ac)
     if a_min == a_max: return 0.0, 0.0, 0.0
-    ps = []
-    lpmax = NINF
-    rpmax = NINF
-    for i in range(a_min, a_max+1):
-        p = _lnProbability(i, ab-i, ac-i, abcd-ab-ac+i)
-        ps.append(p)
-        if i <= a and lpmax < p: lpmax = p
-        if i >= a and rpmax < p: rpmax = p
-        pass
-    tpmax = min(lpmax, rpmax)
-    left_tail = 0
-    right_tail = 0
-    two_tails = 0
-    c = a-a_min
-    for i in range(a_max-a_min+1):
-        if i <= c: left_tail += exp(ps[i]-lpmax)
-        if i >= c: right_tail += exp(ps[i]-rpmax)
-        if ps[i] <= ps[c]: two_tails += exp(ps[i]-tpmax)
-    left_tail = max(0.0, -lpmax-log(left_tail))
-    right_tail = max(0.0, -rpmax-log(right_tail))
-    two_tails = max(0.0, -tpmax-log(two_tails))
+    p0 = lgamma(ab+1) + lgamma(ac+1) + lgamma(abcd-ac+1) \
+         + lgamma(abcd - ab + 1) - lgamma(abcd + 1)
+    ps = [ p0 - lgamma(i+1) - lgamma(ab-i+1) - lgamma(ac-i+1)
+              - lgamma(abcd-ab-ac+i+1)
+           for i in range(a_min, a_max+1) ]
+    lpmax = max(ps[:a-a_min+1])
+    rpmax = max(ps[a-a_min:])
+    tpmax = ps[a-a_min]
+    left_tail = max(0., -lpmax - log(sum( exp(p-lpmax)
+                                          for p in ps[:a-a_min+1] )))
+    right_tail = max(0., -rpmax - log(sum( exp(p-rpmax)
+                                           for p in ps[a-a_min:] )))
+    two_tails = max(0., -tpmax - log(sum( exp(p-tpmax)
+                                          for p in ps if p <= tpmax )))
     return left_tail, right_tail, two_tails
 
 def mlog10Test1(a, b, c, d):
@@ -54,6 +70,7 @@ def mlog10Test1(a, b, c, d):
 def mlog10Test2(a, ab, ac, abcd):
     result = mlnTest2(a, ab, ac, abcd)
     return result[0]/LN10, result[1]/LN10, result[2]/LN10
+
 
 # ======================== Left Tail Only ========================
 
@@ -67,28 +84,24 @@ def mlnTest1l(a, b, c, d):
     return mlnTest2l(a, a+b, a+c, a+b+c+d)
 
 def mlnTest2l(a, ab, ac, abcd):
+    if 0 > a or a > ab or a > ac or ab > abcd or ac > abcd: raise ValueError
     a_min = max(0, ab+ac-abcd)
     a_max = min(ab, ac)
     if a_min == a_max: return 0.0
-    ps = []
-    lpmax = NINF
-    for i in range(a_min, a_max+1):
-        p = _lnProbability(i, ab-i, ac-i, abcd-ab-ac+i)
-        ps.append(p)
-        if i <= a and lpmax < p: lpmax = p
-        pass
-    left_tail = 0
-    c = a-a_min
-    for i in range(a_max-a_min+1):
-        if i <= c: left_tail += exp(ps[i]-lpmax)
-    left_tail = max(0.0, -lpmax-log(left_tail))
-    return left_tail
+    p0 = lgamma(ab+1) + lgamma(ac+1) + lgamma(abcd-ac+1) \
+         + lgamma(abcd - ab + 1) - lgamma(abcd + 1)
+    ps = [ p0 - lgamma(i+1) - lgamma(ab-i+1) - lgamma(ac-i+1)
+              - lgamma(abcd-ab-ac+i+1)
+           for i in range(a_min, a+1) ]
+    lpmax = max(ps)
+    return max(0., -lpmax - log(sum( exp(p-lpmax) for p in ps )))
 
 def mlog10Test1l(a, b, c, d):
     return mlnTest2l(a, a+b, a+c, a+b+c+d)/LN10
 
 def mlog10Test2l(a, ab, ac, abcd):
     return mlnTest2l(a, ab, ac, abcd)/LN10
+
 
 # ======================== Right Tail Only ========================
 
@@ -102,28 +115,24 @@ def mlnTest1r(a, b, c, d):
     return mlnTest2r(a, a+b, a+c, a+b+c+d)
 
 def mlnTest2r(a, ab, ac, abcd):
+    if 0 > a or a > ab or a > ac or ab > abcd or ac > abcd: raise ValueError
     a_min = max(0, ab+ac-abcd)
     a_max = min(ab, ac)
     if a_min == a_max: return 0.0
-    ps = []
-    rpmax = NINF
-    for i in range(a_min, a_max+1):
-        p = _lnProbability(i, ab-i, ac-i, abcd-ab-ac+i)
-        ps.append(p)
-        if i >= a and rpmax < p: rpmax = p
-        pass
-    right_tail = 0
-    c = a-a_min
-    for i in range(a_max-a_min+1):
-        if i >= c: right_tail += exp(ps[i]-rpmax)
-    right_tail = max(0.0, -rpmax-log(right_tail))
-    return right_tail
+    p0 = lgamma(ab+1) + lgamma(ac+1) + lgamma(abcd-ac+1) \
+         + lgamma(abcd - ab + 1) - lgamma(abcd + 1)
+    ps = [ p0 - lgamma(i+1) - lgamma(ab-i+1) - lgamma(ac-i+1)
+              - lgamma(abcd-ab-ac+i+1)
+           for i in range(a, a_max+1) ]
+    rpmax = max(ps)
+    return max(0., -rpmax - log(sum( exp(p-rpmax) for p in ps )))
 
 def mlog10Test1r(a, b, c, d):
     return mlnTest2r(a, a+b, a+c, a+b+c+d)/LN10
 
 def mlog10Test2r(a, ab, ac, abcd):
     return mlnTest2r(a, ab, ac, abcd)/LN10
+
 
 # ======================== Two Tails Only ========================
 
@@ -137,61 +146,29 @@ def mlnTest1t(a, b, c, d):
     return mlnTest2t(a, a+b, a+c, a+b+c+d)
 
 def mlnTest2t(a, ab, ac, abcd):
+    if 0 > a or a > ab or a > ac or ab > abcd or ac > abcd: raise ValueError
     a_min = max(0, ab+ac-abcd)
     a_max = min(ab, ac)
     if a_min == a_max: return 0.0
-    ps = []
-    lpmax = NINF
-    rpmax = NINF
-    for i in range(a_min, a_max+1):
-        p = _lnProbability(i, ab-i, ac-i, abcd-ab-ac+i)
-        ps.append(p)
-        if i <= a and lpmax < p: lpmax = p
-        if i >= a and rpmax < p: rpmax = p
-        pass
-    tpmax = min(lpmax, rpmax)
-    two_tails = 0
-    c = a-a_min
-    for i in range(a_max-a_min+1):
-        if ps[i] <= ps[c]: two_tails += exp(ps[i]-tpmax)
-    two_tails = max(0.0, -tpmax-log(two_tails))
-    return two_tails
+    p0 = lgamma(ab+1) + lgamma(ac+1) + lgamma(abcd-ac+1) \
+         + lgamma(abcd - ab + 1) - lgamma(abcd + 1)
+    tpmax = p0 - lgamma(a+1) - lgamma(ab-a+1) - lgamma(ac-a+1) \
+               - lgamma(abcd-ab-ac+a+1)
+    s = 1.
+    for i in range(a_min, a):
+        p = p0 - lgamma(i+1) - lgamma(ab-i+1) - lgamma(ac-i+1) \
+               - lgamma(abcd-ab-ac+i+1)
+        if p > tpmax: break
+        s += exp(p-tpmax)
+    for i in range(a_max, a, -1):
+        p = p0 - lgamma(i+1) - lgamma(ab-i+1) - lgamma(ac-i+1) \
+               - lgamma(abcd-ab-ac+i+1)
+        if p > tpmax: break
+        s += exp(p-tpmax)
+    return max(0., -tpmax - log(s))
 
 def mlog10Test1t(a, b, c, d):
     return mlnTest2t(a, a+b, a+c, a+b+c+d)/LN10
 
 def mlog10Test2t(a, ab, ac, abcd):
     return mlnTest2t(a, ab, ac, abcd)/LN10
-
-# ======================== Common ========================
-
-try:
-
-    from math import lgamma  # Python 2.7+
-
-    def _lnProbability (a, b, c, d):
-        return (lgamma(a+b+1)+lgamma(c+d+1)+lgamma(a+c+1)+lgamma(b+d+1)
-                -lgamma(a+1)-lgamma(b+1)-lgamma(c+1)-lgamma(d+1)-lgamma(a+b+c+d+1))
-
-except ImportError:
-
-    _cache = {0: 0, 1: 0}
-
-    def _lnGamma (z):
-        if z in _cache: return _cache[z]
-        x = 0
-        x += 0.1659470187408462e-06 / (z + 8)
-        x += 0.9934937113930748e-05 / (z + 7)
-        x -= 0.1385710331296526 / (z + 6)
-        x += 12.50734324009056 / (z + 5)
-        x -= 176.6150291498386 / (z + 4)
-        x += 771.3234287757674 / (z + 3)
-        x -= 1259.139216722289 / (z + 2)
-        x += 676.5203681218835 / (z + 1)
-        x += 0.9999999999995183
-        _cache[z] = x = log(x) - 6.58106146679532777 - z + (z + 0.5) * log(z + 7.5)
-
-    def _lnProbability (a, b, c, d):
-        return (_lnGamma(a+b)+_lnGamma(c+d)+_lnGamma(a+c)+_lnGamma(b+d)
-                -_lnGamma(a)-_lnGamma(b)-_lnGamma(c)-_lnGamma(d)-_lnGamma(a+b+c+d))
-        return x
---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: eph
+
+import unittest
+from random import randrange
+
+import fisher
+
+
+class TestFisher(unittest.TestCase):
+
+    def test_with_scipy(self, samples=100):
+        from scipy.stats import fisher_exact
+        for _ in range(samples):
+            a = randrange(randrange(randrange(randrange(65536)+1)+1)+1)
+            b = randrange(randrange(randrange(randrange(65536)+1)+1)+1)
+            c = randrange(randrange(randrange(randrange(65536)+1)+1)+1)
+            d = randrange(randrange(randrange(randrange(65536)+1)+1)+1)
+            lp, rp, tp = fisher.test1(a, b, c, d)
+            self.assertAlmostEqual(lp, fisher_exact([[a, b], [c, d]], 'less')[1])
+            self.assertAlmostEqual(rp, fisher_exact([[a, b], [c, d]], 'greater')[1])
+            self.assertAlmostEqual(tp, fisher_exact([[a, b], [c, d]], 'two-sided')[1])
+            self.assertAlmostEqual(lp, fisher.test1l(a, b, c, d))
+            self.assertAlmostEqual(rp, fisher.test1r(a, b, c, d))
+            self.assertAlmostEqual(tp, fisher.test1t(a, b, c, d))
+
+if __name__ == '__main__':
+    unittest.main()
