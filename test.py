33c966ad8e542ad21411d91505433b16782b6a27
eph <painy.eph@gmail.com>
2017-02-14 22:13:00 +0800

    improved random test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 # Author: eph
 
 import unittest
-from random import randrange
+from random import random
 
 import fisher
 
@@ -13,10 +13,10 @@ class TestFisher(unittest.TestCase):
     def test_with_scipy(self, samples=100):
         from scipy.stats import fisher_exact
         for _ in range(samples):
-            a = randrange(randrange(randrange(randrange(65536)+1)+1)+1)
-            b = randrange(randrange(randrange(randrange(65536)+1)+1)+1)
-            c = randrange(randrange(randrange(randrange(65536)+1)+1)+1)
-            d = randrange(randrange(randrange(randrange(65536)+1)+1)+1)
+            a = int(10**(random()*4) - 1)
+            b = int(10**(random()*4) - 1)
+            c = int(10**(random()*4) - 1)
+            d = int(10**(random()*4) - 1)
             lp, rp, tp = fisher.test1(a, b, c, d)
             self.assertLessEqual(lp, 1)
             self.assertLessEqual(rp, 1)
